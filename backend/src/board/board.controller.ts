f5d1230d2a18cdc3a9049a4577fa65a5fe4ae64a
Kim Yoo <kimyoo04eco@naver.com>
2023-08-01 05:02:20 +0900

    feat: board 컨트롤러 CRUD 초기 설정

--- backend/src/board/board.controller.ts
+++ backend/src/board/board.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  @Get()
+  findAll() {
+    return 'find All';
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return `find ${id}`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    console.log(body);
+    return 'create';
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body: any) {
+    console.log(id, body);
+    return 'update';
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return `remove ${id}`;
+  }
+}
