799cc1d7e984c22b342f7187aafc744a9fb540b8
Kim Yoo <kimyoo04eco@naver.com>
2023-08-01 04:19:09 +0900

    fix: single quote 사용하도록 설정 및 board 컨트롤러 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BoardController } from './board/board.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BoardController],
   providers: [AppService],
 })
 export class AppModule {}
