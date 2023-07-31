a67c92955b580c6a6267624b6cbe7e0c1872bf7c
Kim Yoo <kimyoo04eco@naver.com>
2023-08-01 05:06:49 +0900

    feat: board 모듈, 서비스 생성 및 설정

--- backend/src/board/board.controller.ts
+++ backend/src/board/board.controller.ts
@@ -7,9 +7,12 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board')
 export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
   @Get()
   findAll() {
     return 'find All';
