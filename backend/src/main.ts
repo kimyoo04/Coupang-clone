c134fd9b7f9a1db4b570acd44b4f2521f6823764
Kim Yoo <kimyoo04eco@naver.com>
2023-08-01 05:47:11 +0900

    feat: swagger 설정 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Coupang-clone API')
+    .setDescription('Coupang-clone description')
+    .setVersion('1.0')
+    .addTag('Board')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(9000);
 }
 bootstrap();
