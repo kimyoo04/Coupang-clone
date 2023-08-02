7defc79fa2816b5106c9b153130a18545d534966
Kim Yoo <kimyoo04eco@naver.com>
2023-08-02 11:19:58 +0900

    feat: postgreSQL, TypeORM 설정 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,25 @@ import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { LoggingMiddleware } from './middleware/logging-middleware';
 import ConfigModule from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule(), BoardModule],
+  imports: [
+    ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    BoardModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
