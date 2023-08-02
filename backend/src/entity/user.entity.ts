dcb1e490fd2415804e9d001f952d6b4f24cf0826
Kim Yoo <kimyoo04eco@naver.com>
2023-08-02 11:24:57 +0900

    feat: user route 생성 (co, mo, s) 및 User 엔티티 생성

---
+++ backend/src/entity/user.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+  OneToMany,
+} from 'typeorm';
+import { Board } from './board.entity';
+
+@Entity()
+@Unique(['email']) // 이메일은 중복되지 않아야 합니다.
+export class User {
+  // @PrimaryColumn('uuid', { default: uuidv4() }) // 기본값으로 UUID를 생성
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  emailVerified: boolean;
+
+  @Column({ nullable: true })
+  oauthProvider: string; // OAuth 제공자 (ex: Google, Facebook 등)
+
+  @Column({ nullable: true })
+  oauthId: string; // OAuth 제공자에서의 사용자 고유 ID
+
+  @Column({ nullable: true })
+  profilePictureUrl: string; // OAuth 제공자에서 받아온 프로필 사진 URL
+
+  // Board와의 One-to-Many 관계 설정
+  @OneToMany(() => Board, (board) => board.user)
+  boards: Board[]; // User 엔티티와의 관계를 나타내는 필드
+}
