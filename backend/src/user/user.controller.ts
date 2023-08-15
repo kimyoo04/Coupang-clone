47ce6dae0d990befbee0986607de35b89baf1dcc
Kim Yoo <kimyoo04eco@naver.com>
2023-08-16 01:11:36 +0900

    feat: user 컨트롤러에 resDto 적용

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,17 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { ApiGetResponse } from '@/common/decorator/swagger.decorator';
+import { FindUserResDto } from './dto/res.dto';
 
 @ApiTags('User')
+@ApiExtraModels(FindUserResDto)
 @Controller('api/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
+  @ApiGetResponse(FindUserResDto)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
