996d9a28e19c273e8fb8bcf2085f4a0cd825d783
Kim Yoo <kimyoo04eco@naver.com>
2023-08-15 23:34:39 +0900

    feat: auth를 위한 req.dto.ts 추가 및 useGlobalPipes 적용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,14 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  // ValidationPipe 전역 적용
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // class-transformer 적용
+      transform: true,
+    }),
+  );
 
   await app.listen(8000);
 }
