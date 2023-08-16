1b7001439d8694cbbed558874c1e806608da4dcd
Kim Yoo <kimyoo04eco@naver.com>
2023-08-17 01:26:18 +0900

    style: 주석 수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -29,7 +29,7 @@ import jwtConfig from './config/jwt.config';
           password: configService.get('postgres.password'),
           autoLoadEntities: true,
         };
-        // 주의! development 환경에서만 개발 편의성을 위해 활용
+        // 주의! development 환경에서만 db 활용
         if (configService.get('NODE_ENV') === 'development') {
           console.info('Sync TypeORM');
           obj = Object.assign(obj, {
