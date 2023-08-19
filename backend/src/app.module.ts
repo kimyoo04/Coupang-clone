4827e0e8c43c6f840aa5af8788ffe938870afb7e
Kim Yoo <kimyoo04eco@naver.com>
2023-08-19 11:54:22 +0900

    feat: env.config.ts 생성 및 environment 환경 변수 적용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,15 +7,17 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AnalyticsModule } from './analytics/analytics.module';
+
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import postgresConfig from './config/postgres.config';
 import jwtConfig from './config/jwt.config';
+import envConfig from './config/env.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [postgresConfig, jwtConfig],
+      load: [postgresConfig, jwtConfig, envConfig],
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
@@ -30,7 +32,7 @@ import jwtConfig from './config/jwt.config';
           autoLoadEntities: true,
         };
         // 주의! development 환경에서만 db 활용
-        if (configService.get('NODE_ENV') === 'development') {
+        if (configService.get('ENVIRONMENT') === 'development') {
           console.info('Sync TypeORM');
           obj = Object.assign(obj, {
             synchronize: true,
